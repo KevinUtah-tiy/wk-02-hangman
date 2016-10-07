f2a0dbae6a8856bc3160929d901b4e70a160ecb6
Kevin Black <Kevin_Jay_Black@hotmail.com>
2016-10-07 15:39:04 -0600

    feat(core) Modify array for commonWords

---
+++ js/common-words.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var commonWords = [
+  "the","and","you","that",
+  "was","for","are","with","his","they",
+  "this","have","from","one","had","word","but","not",
+  "what","all","were","when","your","can","said","there",
+  "use","each","which","she","how","their","will","other","about","out","many","then","them","these",
+  "some","her","would","make","like","him","into","time","has","look","two","more","write","see","number","way","could","people","than","first","water","been","call","who","oil",
+  "its","now","find","long","down","day","did","get","come","made","may","part"
+];
+
+function commonWords() { commonWords[Math.floor(Math.random()*commonWords.length)]
+return true;
+}
