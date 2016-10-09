d869a7299869d45b46088c7dd6df48149d5cc528
Kevin Black <Kevin_Jay_Black@hotmail.com>
2016-10-09 10:47:43 -0600

    feat(core) JS: Create body and limbs through functions

--- js/main.js
+++ js/main.js
@@ -129,6 +129,28 @@ canvas = function() {
     draw (60, 5, 60, 15);
   };
 
+  torso = function() {
+    draw (60, 36, 60, 70);
+  };
+
+  rightArm = function() {
+    draw (60, 46, 100, 50);
+  };
+
+  leftArm = function() {
+    draw (60, 46, 20, 50);
+  };
+
+  rightLeg = function () {
+    draw (60, 70, 100, 100);
+  };
+
+  leftLeg = function () {
+    draw (60, 70, 20, 100);
+  };
+
+  drawArray = [rightLeg, leftLeg, rightArm, leftArm, torso, head, frame4, frame3, frame2, frame1];
+
 
 
 
