9c8eebe69c852def482bff3e2ff53ecbe6e7d39f
Kevin Black <Kevin_Jay_Black@hotmail.com>
2016-10-09 10:02:32 -0600

    feat(core) JS: Rename file; Add categories
    File was renamed to reflect directions from instructor
    There are three categories to randomly select from

--- js/main.js
+++ js/main.js
@@ -37,20 +37,35 @@ var buttons = document.getElementById() {
   }
 }
 
+//Have the category randomly chosen by browser
+var givenCat = function() {
+  if(givenCategory === categories[0])  {
+    categoryName.innerHTML = "The Category is NFL Team Names";
+  } else if(givenCategory) === categories[1] {
+    categoryName.innerHTML = "The Category is State Capitals";
+  } else if(givenCategory === categories[2]) {
+    "The Category is Chemical Elements"
+  } // closes the final if statement
+} //closes the var givenCat function
 
 
 
 
-var commonWords = [
-  "the","and","you","that",
-  "was","for","are","with","his","they",
-  "this","have","from","one","had","word","but","not",
-  "what","all","were","when","your","can","said","there",
-  "use","each","which","she","how","their","will","other","about","out","many","then","them","these",
-  "some","her","would","make","like","him","into","time","has","look","two","more","write","see","number","way","could","people","than","first","water","been","call","who","oil",
-  "its","now","find","long","down","day","did","get","come","made","may","part"
-];
 
-function commonWords() { commonWords[Math.floor(Math.random()*commonWords.length)]
-return true;
-}
+
+
+/* -------------- end here ------------- */
+
+// var commonWords = [
+//   "the","and","you","that",
+//   "was","for","are","with","his","they",
+//   "this","have","from","one","had","word","but","not",
+//   "what","all","were","when","your","can","said","there",
+//   "use","each","which","she","how","their","will","other","about","out","many","then","them","these",
+//   "some","her","would","make","like","him","into","time","has","look","two","more","write","see","number","way","could","people","than","first","water","been","call","who","oil",
+//   "its","now","find","long","down","day","did","get","come","made","may","part"
+// ];
+//
+// function commonWords() { commonWords[Math.floor(Math.random()*commonWords.length)]
+// return true;
+// }
