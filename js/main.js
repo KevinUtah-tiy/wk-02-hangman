68064ebc3a5023bf4c33fde31d4314eeeeb7097a
Kevin Black <Kevin_Jay_Black@hotmail.com>
2016-10-09 21:26:14 -0600

    WIP - Modify style for header, body and footer

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 // window.onload = console.log(“It works!”);
 
-window.onLoad = function();
+window.onLoad = function() {
 
 //Define alphabet as an array
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
