2d673a0b29d244f62932730b34961b7a2572d3e6
Kevin Black <Kevin_Jay_Black@hotmail.com>
2016-10-09 22:53:36 -0600

    feat(core) WIP - Modify JavaScript to link with Browser

--- js/main.js
+++ js/main.js
@@ -245,18 +245,3 @@ document.getElementById('reset').onclick = function() {
 
 
 /* -------------- end here ------------- */
-
-// var commonWords = [
-//   "the","and","you","that",
-//   "was","for","are","with","his","they",
-//   "this","have","from","one","had","word","but","not",
-//   "what","all","were","when","your","can","said","there",
-//   "use","each","which","she","how","their","will","other","about","out","many","then","them","these",
-//   "some","her","would","make","like","him","into","time","has","look","two","more","write","see","number","way","could","people","than","first","water","been","call","who","oil",
-//   "its","now","find","long","down","day","did","get","come","made","may","part"
-// ];
-//
-// function commonWords() { commonWords[Math.floor(Math.random()*commonWords.length)]
-// return true;
-// }
-// window.onload = console.log(“It works!”);
